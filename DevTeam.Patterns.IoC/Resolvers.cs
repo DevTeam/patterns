1b0c16a26b94bd2f95cc855fe203ce859dc9f764
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-05-16 12:40:30 +0300

    Add steps for creating and pushing NuGet packages

---
+++ DevTeam.Patterns.IoC/Resolvers.cs
@@ -0,0 +1,24 @@
+﻿namespace DevTeam.Patterns.IoC
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Resolvers
+    {
+        public static async Task<T> Resolve<T>(this IResolver resolver, string name = "")
+        {
+            if (resolver == null) throw new ArgumentNullException(nameof(resolver));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return await resolver.Resolve<Task<T>>(name);
+        }
+
+        public static async Task<T> Resolve<TArg, T>(this IResolver resolver, TArg arg, string name = "")
+        {
+            if (resolver == null) throw new ArgumentNullException(nameof(resolver));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return await resolver.Resolve<TArg, Task<T>>(arg, name);
+        }
+    }
+}
