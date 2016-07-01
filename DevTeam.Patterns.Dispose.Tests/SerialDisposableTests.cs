c1ad7f487b323dbf8c4f9c2690ee32f425e71ca0
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-07-01 16:42:41 +0300

    Fix build

---
+++ DevTeam.Patterns.Dispose.Tests/SerialDisposableTests.cs
@@ -0,0 +1,115 @@
+﻿namespace DevTeam.Patterns.Dispose.Tests
+{
+	using System;
+
+	using Moq;
+
+	using NUnit.Framework;
+
+	using Shouldly;
+
+	[TestFixture]
+	public class SerialDisposableTests
+    {
+		private Mock<IDisposable> _disposable;
+		private Mock<IDisposable> _disposable2;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_disposable = new Mock<IDisposable>();
+			_disposable2 = new Mock<IDisposable>();
+		}
+
+		[Test]
+		public void ShouldAssignDisposableWhenCtor()
+		{
+            // Given
+
+            // When
+            var target = CreateTarget(_disposable.Object);
+
+            // Then
+            target.Disposable.ShouldBe(_disposable.Object);
+		}
+
+        [Test]
+        public void ShouldAssignDisposableWhenSetter()
+        {
+            // Given
+            var target = CreateTarget();
+
+            // When   
+            target.Disposable = _disposable.Object;
+
+            // Then
+            target.Disposable.ShouldBe(_disposable.Object);
+        }
+
+        [Test]
+        public void ShouldHasDefaultDisposable()
+        {
+            // Given            
+
+            // When   
+            var target = CreateTarget();
+
+            // Then
+            target.Disposable.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void ShouldDisposePrevDisposableWhenChanged()
+        {
+            // Given
+            var target = CreateTarget(_disposable.Object);
+
+            // When            
+            target.Disposable = _disposable2.Object;
+
+            // Then
+            _disposable.Verify(i => i.Dispose());
+            target.Disposable.ShouldBe(_disposable2.Object);
+        }
+
+        [Test]
+        public void ShouldDisposeAfterSetWhenDisposed()
+        {
+            // Given
+            var target = CreateTarget();
+
+            // When   
+            target.Dispose();
+            target.Disposable = _disposable.Object;
+
+            // Then
+            _disposable.Verify(i => i.Dispose());
+            target.Disposable.ShouldBe(_disposable.Object);
+        }
+
+        [Test]
+        public void ShouldDisposWhenDisposed()
+        {
+            // Given
+            var target = CreateTarget();
+
+            // When   
+            target.Disposable = _disposable.Object;
+            target.Dispose();
+
+            // Then
+            _disposable.Verify(i => i.Dispose());
+            target.Disposable.ShouldBe(_disposable.Object);
+        }
+
+        private static SerialDisposable CreateTarget(IDisposable disposable)
+		{
+			return new SerialDisposable(disposable);
+		}
+
+        private static SerialDisposable CreateTarget()
+        {
+            return new SerialDisposable();
+        }
+    }
+}
---
+++ DevTeam.Patterns.Dispose/SerialDisposable.cs
@@ -0,0 +1,50 @@
+﻿namespace DevTeam.Patterns.Dispose
+{
+    using System;
+
+    public class SerialDisposable: IDisposable
+    {
+        private IDisposable _disposable = Patterns.Dispose.Disposable.Empty();
+
+        public SerialDisposable()            
+        {
+        }
+
+        public SerialDisposable(IDisposable disposable)
+        {
+            if (disposable == null) throw new ArgumentNullException(nameof(disposable));
+
+            Disposable = disposable;
+        }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+            Disposable.Dispose();
+        }
+
+        public IDisposable Disposable
+        {
+            get
+            {
+                return _disposable;
+            }
+
+            set
+            {
+                if (IsDisposed)
+                {
+                    _disposable = value;
+                    _disposable.Dispose();
+                }
+                else
+                {
+                    _disposable.Dispose();
+                    _disposable = value;
+                }                
+            }
+        }
+
+        public bool IsDisposed { get; private set; }
+    }
+}
