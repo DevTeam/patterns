001955baafa1fb63a3845ccf8e6ed065f43b4408
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-07-07 18:34:57 +0300

    Add .net 4.5 test mocks

---
+++ TestTool/DevTeam.TestTool.Test.Mocks2/TestFixtureSuccess.cs
@@ -0,0 +1,13 @@
+﻿namespace DevTeam.TestTool.Test.Mocks2
+{
+    using Contracts;
+
+    [TestFixture]
+    public class TestFixtureSuccess
+    {
+        [Test]
+        public void TestSuccess()
+        {
+        }
+    }
+}
